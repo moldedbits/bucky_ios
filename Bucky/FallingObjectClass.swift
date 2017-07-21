3a876133c6c54df1da691e7924a308b1ef39020a
Jeeshan Ali <jeeshan@Jeeshans-Mac.local>
2017-07-21 12:08:09 +0530

    create Falling Object

---
+++ Bucky/FallingObjectClass.swift
@@ -0,0 +1,27 @@
+//
+//  FallingObjectClass.swift
+//  Bucky
+//
+//  Created by Jeeshan Ali on 21/07/17.
+//  Copyright © 2017 moldedbits. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol FallingObjectDelegate {
+    
+}
+
+class FallingObject  {
+    let arr[:] = UIImageView() : score
+    let selected
+    let score
+    
+    let ballView = BallView()
+    let bucketView = BucketView()
+    
+    func isIntersect() {
+        
+    }
+}
