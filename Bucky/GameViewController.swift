d04ca33b2f4afdb55c71a3df625c73a20853deee
vaibhavsingh911@gmail.com <vaibhavsingh911@gmail.com>
2017-07-21 12:41:27 +0530

    Add gameViewController Files with the .xib

---
+++ Bucky/GameViewController.swift
@@ -0,0 +1,28 @@
+//
+//  GameViewController.swift
+//  Bucky
+//
+//  Created by Vaibhav Singh on 21/07/17.
+//  Copyright © 2017 moldedbits. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
