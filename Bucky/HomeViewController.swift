97777d3db8a10dfc72949e510670dc18d58a517b
7rcksh@gmail.com <7rcksh@gmail.com>
2017-07-24 15:40:37 +0530

    Refactor code.

--- Bucky/HomeViewController.swift
+++ Bucky/HomeViewController.swift
@@ -38,8 +38,8 @@ class HomeViewController: UIViewController {
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        UIView.animate(withDuration: 0.7, delay: 0.3, options: [.autoreverse,.repeat,.allowUserInteraction],animations: {
-        self.playGameButton.transform = CGAffineTransform(scaleX: 0.6, y: 0.6)
+        UIView.animate(withDuration: 0.7, delay: 0, options: [.autoreverse,.repeat,.allowUserInteraction],animations: {
+        self.playGameButton.transform = CGAffineTransform(scaleX: 1.5, y: 1.5)
         },completion: nil)
     }
     
